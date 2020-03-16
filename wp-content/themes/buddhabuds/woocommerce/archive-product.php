5239936bf1c39ae6588a71648334b6e500ef5281
Matt <matt@mspacecreative.com>
2020-03-16 15:42:44 -0300

    c

--- wp-content/themes/buddhabuds/woocommerce/archive-product.php
+++ wp-content/themes/buddhabuds/woocommerce/archive-product.php
@@ -104,36 +104,46 @@ do_action( 'woocommerce_sidebar' ); ?>
 
 <div id="contact" class="et_pb_section et_pb_section_2 et_pb_section_parallax et_pb_with_background et_section_regular">
 				
-				<div class="et_parallax_bg_wrap"><div class="et_parallax_bg" style="background-image: url(&quot;http://buddhabuds.urbanbuilt.ca/wp-content/uploads/2020/03/buddha_buds_halifax_cannabis.jpg&quot;); height: 598.0999999999999px; transform: translate(0px, 111.89999999999999px);"></div></div>
+	<div class="et_parallax_bg_wrap">
+		<div class="et_parallax_bg" style="background-image: url(&quot;http://buddhabuds.urbanbuilt.ca/wp-content/uploads/2020/03/buddha_buds_halifax_cannabis.jpg&quot;); height: 598.0999999999999px; transform: translate(0px, 111.89999999999999px);"></div>
+	</div>
 				
 				
-					<div class="et_pb_row et_pb_row_0 et_pb_row_fullwidth et_pb_equal_columns et_pb_gutters1">
-				<div class="et_pb_column et_pb_column_1_2 et_pb_column_2  et_pb_css_mix_blend_mode_passthrough">
+	<div class="et_pb_row et_pb_row_0 et_pb_row_fullwidth et_pb_equal_columns et_pb_gutters1">
+		<div class="et_pb_column et_pb_column_1_2 et_pb_column_2  et_pb_css_mix_blend_mode_passthrough">
 				
-				
-				<div class="et_pb_module et_pb_text et_pb_text_0  et_pb_text_align_left et_pb_bg_layout_dark">
-				
-				
-				<div class="et_pb_text_inner"><h2>Contact Us</h2></div>
-			</div> <!-- .et_pb_text --><div class="et_pb_button_module_wrapper et_pb_button_0_wrapper  et_pb_module ">
-				<a class="et_pb_button et_pb_button_0 et_pb_bg_layout_light" href="mailto:info@buddhabudshfx.com">info@buddhabudshfx.com</a>
+			<div class="et_pb_module et_pb_text et_pb_text_0  et_pb_text_align_left et_pb_bg_layout_dark">
+				<div class="et_pb_text_inner">
+					<h2>Contact Us</h2>
+				</div>
 			</div>
-			</div> <!-- .et_pb_column --><div class="et_pb_column et_pb_column_1_2 et_pb_column_3  et_pb_css_mix_blend_mode_passthrough et-last-child">
-				
-				
-				<div class="et_pb_module et_pb_text et_pb_text_1  et_pb_text_align_left et_pb_bg_layout_dark">
-				
-				
-				<div class="et_pb_text_inner"><h2>My account</h2></div>
-			</div> <!-- .et_pb_text --><div class="et_pb_button_module_wrapper et_pb_button_1_wrapper  et_pb_module ">
-				<a class="et_pb_button et_pb_button_1 home_login-register et_pb_bg_layout_light" href="/my-account/">Go to dashboard</a>
+			
+			<div class="et_pb_button_module_wrapper et_pb_button_0_wrapper  et_pb_module ">
+				<a class="et_pb_button et_pb_button_0 et_pb_bg_layout_light" href="mailto:info@buddhabudshfx.com">info@buddhabudshfx.com
+				</a>
 			</div>
-			</div> <!-- .et_pb_column -->
+			
+		</div>
+			
+		<div class="et_pb_column et_pb_column_1_2 et_pb_column_3  et_pb_css_mix_blend_mode_passthrough et-last-child">
 				
+			<div class="et_pb_module et_pb_text et_pb_text_1  et_pb_text_align_left et_pb_bg_layout_dark">
 				
-			</div> <!-- .et_pb_row -->
+				<div class="et_pb_text_inner">
+					<h2>My account</h2>
+				</div>
 				
+				<div class="et_pb_button_module_wrapper et_pb_button_1_wrapper et_pb_module">
+					<a class="et_pb_button et_pb_button_1 home_login-register et_pb_bg_layout_light" href="/my-account/">Go to dashboard</a>
+				</div>
 				
 			</div>
+			
+		</div>
+				
+				
+	</div>
+				
+</div>
 
 <?php get_footer( 'shop' ); ?>
