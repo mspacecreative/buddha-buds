ea27e61ebec19ad7ac1af20e4a1ba895bf691811
Matt <matt@mspacecreative.com>
2020-03-16 11:30:16 -0300

    c

--- wp-content/themes/buddhabuds/woocommerce/loop/price.php
+++ wp-content/themes/buddhabuds/woocommerce/loop/price.php
@@ -24,4 +24,22 @@ global $product;
 
 <?php if ( $price_html = $product->get_price_html() ) : ?>
 	<span class="price"><?php echo $price_html; _e(' / gram'); ?></span>
-<?php endif; ?>
+<?php endif;
+
+function sv_change_product_html( $price_html, $product ) {
+	if ( 252 === $product->id ) {
+		$price_html = '<span class="amount">$15.00 per kg</span>';	
+	}
+	
+	return $price_html;
+}
+add_filter( 'woocommerce_get_price_html', 'sv_change_product_html', 10, 2 );
+
+
+function sv_change_product_price_cart( $price, $cart_item, $cart_item_key ) {
+	if ( 252 === $cart_item['product_id'] ) {
+		$price = '$15.00 per kg<br>(7-8 skewers per kg)';
+	}
+	return $price;
+}	
+add_filter( 'woocommerce_cart_item_price', 'sv_change_product_price_cart', 10, 3 );
