effdb28ed0d721cc63726b5521de4672fb8e4e2b
Matt <matt@mspacecreative.com>
2020-03-16 15:50:47 -0300

    c

--- wp-content/themes/buddhabuds/woocommerce/archive-product.php
+++ wp-content/themes/buddhabuds/woocommerce/archive-product.php
@@ -105,12 +105,12 @@ do_action( 'woocommerce_sidebar' ); ?>
 <div id="contact" class="et_pb_section et_pb_section_2 et_pb_section_parallax et_pb_with_background et_section_regular">
 				
 	<div class="et_parallax_bg_wrap">
-		<div class="et_parallax_bg" style="background-image: url(../../../../wp-content/uploads/2020/03/buddha_buds_halifax_cannabis.jpg&quot;); height: 598.0999999999999px; transform: translate(0px, 111.89999999999999px);"></div>
+		<div class="et_parallax_bg" style="background-image: url(../../../../wp-content/uploads/2020/03/buddha_buds_halifax_cannabis.jpg); height: 598.0999999999999px; transform: translate(0px, 111.89999999999999px);"></div>
 	</div>
 				
 				
 	<div class="et_pb_row et_pb_row_0 et_pb_row_fullwidth et_pb_equal_columns et_pb_gutters1">
-		<div class="et_pb_column et_pb_column_1_2 et_pb_column_2  et_pb_css_mix_blend_mode_passthrough" style="background-color: rgba(0,0,0,0.6);">
+		<div class="et_pb_column et_pb_column_1_2 et_pb_column_2  et_pb_css_mix_blend_mode_passthrough" style="background-color: rgba(0,0,0,0.6); padding-top: 100px; padding-right: 10%; padding-bottom: 100px; padding-left: 10%;">
 				
 			<div class="et_pb_module et_pb_text et_pb_text_0  et_pb_text_align_left et_pb_bg_layout_dark">
 				<div class="et_pb_text_inner">
@@ -125,7 +125,7 @@ do_action( 'woocommerce_sidebar' ); ?>
 			
 		</div>
 			
-		<div class="et_pb_column et_pb_column_1_2 et_pb_column_3  et_pb_css_mix_blend_mode_passthrough et-last-child" style="background-color: #414042;">
+		<div class="et_pb_column et_pb_column_1_2 et_pb_column_3  et_pb_css_mix_blend_mode_passthrough et-last-child" style="background-color: #414042; padding-top: 100px; padding-right: 10%; padding-bottom: 100px; padding-left: 10%;">
 				
 			<div class="et_pb_module et_pb_text et_pb_text_1  et_pb_text_align_left et_pb_bg_layout_dark">
 				
