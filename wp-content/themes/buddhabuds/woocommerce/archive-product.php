76788052eeff9ca0559e2f93462ed40644ace205
Matt <matt@mspacecreative.com>
2020-03-16 15:52:57 -0300

    c

--- wp-content/themes/buddhabuds/woocommerce/archive-product.php
+++ wp-content/themes/buddhabuds/woocommerce/archive-product.php
@@ -102,14 +102,14 @@ do_action( 'woocommerce_after_main_content' );
  */
 do_action( 'woocommerce_sidebar' ); ?>
 
-<div id="contact" class="et_pb_section et_pb_section_2 et_pb_section_parallax et_pb_with_background et_section_regular">
+<div id="contact" class="et_pb_section et_pb_section_2 et_pb_section_parallax et_pb_with_background et_section_regular" style="padding: 0;">
 				
 	<div class="et_parallax_bg_wrap">
 		<div class="et_parallax_bg" style="background-image: url(../../../../wp-content/uploads/2020/03/buddha_buds_halifax_cannabis.jpg); height: 598.0999999999999px; transform: translate(0px, 111.89999999999999px);"></div>
 	</div>
 				
 				
-	<div class="et_pb_row et_pb_row_0 et_pb_row_fullwidth et_pb_equal_columns et_pb_gutters1">
+	<div class="et_pb_row et_pb_row_0 et_pb_row_fullwidth et_pb_equal_columns et_pb_gutters1" style="padding: 0; width: 100%;">
 		<div class="et_pb_column et_pb_column_1_2 et_pb_column_2  et_pb_css_mix_blend_mode_passthrough" style="background-color: rgba(0,0,0,0.6); padding-top: 100px; padding-right: 10%; padding-bottom: 100px; padding-left: 10%;">
 				
 			<div class="et_pb_module et_pb_text et_pb_text_0  et_pb_text_align_left et_pb_bg_layout_dark">
