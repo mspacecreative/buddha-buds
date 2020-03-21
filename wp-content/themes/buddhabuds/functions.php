71dc5e25b02d678fcedbd67311cd3dc38e171e7a
Matt <matt@mspacecreative.com>
2020-03-21 14:33:11 -0300

    c

--- wp-content/themes/buddhabuds/functions.php
+++ wp-content/themes/buddhabuds/functions.php
@@ -221,7 +221,7 @@ function so174837_registration_email_alert( $user_id ) {
     $user    = get_userdata( $user_id );
     $email   = $user->user_email;
     $message = 'A new customer, ' . $email . ', has registered their account on Buddha Buds.';
-    wp_mail( 'orders@buddhabudshfx.com', 'New custom registration', $message );
+    wp_mail( 'orders@buddhabudshfx.com', 'New customer registration', $message );
 }
 add_action('user_register', 'so174837_registration_email_alert');
 
