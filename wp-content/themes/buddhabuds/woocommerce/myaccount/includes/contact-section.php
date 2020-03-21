6b3aef3332b26cb075919bf00dd8cb50c5b661d1
Matt <matt@mspacecreative.com>
2020-03-21 15:16:50 -0300

    c

---
+++ wp-content/themes/buddhabuds/woocommerce/myaccount/includes/contact-section.php
@@ -0,0 +1,69 @@
+<div id="contact" class="et_pb_section et_pb_section_2 et_pb_section_parallax et_pb_with_background et_section_regular" style="padding: 0;">
+				
+	<div class="et_parallax_bg_wrap">
+		<div class="et_parallax_bg" style="background-image: url(../../../../wp-content/uploads/2020/03/buddha_buds_halifax_cannabis.jpg); height: 598.0999999999999px; transform: translate(0px, 111.89999999999999px);"></div>
+	</div>
+				
+				
+	<div class="et_pb_row et_pb_row_0 et_pb_row_fullwidth et_pb_equal_columns et_pb_gutters1">
+		<div class="left_column et_pb_column et_pb_column_1_2 et_pb_column_2  et_pb_css_mix_blend_mode_passthrough">
+				
+			<div class="et_pb_module et_pb_text et_pb_text_0  et_pb_text_align_left et_pb_bg_layout_dark">
+				<div class="et_pb_text_inner">
+					<h2>Contact Us</h2>
+				</div>
+			</div>
+			
+			<div class="et_pb_button_module_wrapper et_pb_button_0_wrapper et_pb_module">
+				<a class="et_pb_button et_pb_button_0 et_pb_bg_layout_light" href="mailto:info@buddhabudshfx.com">info@buddhabudshfx.com
+				</a>
+			</div>
+			
+		</div>
+			
+		<div class="right_column et_pb_column et_pb_column_1_2 et_pb_column_3  et_pb_css_mix_blend_mode_passthrough et-last-child">
+				
+			<div class="et_pb_module et_pb_text et_pb_text_1  et_pb_text_align_left et_pb_bg_layout_dark">
+				
+				<div class="et_pb_text_inner">
+					<h2>My account</h2>
+				</div>
+				
+				<div class="et_pb_button_module_wrapper et_pb_button_1_wrapper et_pb_module">
+					<a class="et_pb_button et_pb_button_1 home_login-register et_pb_bg_layout_light" href="/my-account/">Go to dashboard</a>
+				</div>
+				
+			</div>
+			
+		</div>
+				
+				
+	</div>
+				
+</div>
+
+<style>
+	.et-db #et-boc .et-l #contact .et_pb_row {
+		padding: 0;
+		width: 100%;
+		max-width: 100%;
+	}
+	.left_column {
+		background-color: rgba(0,0,0,0.6);
+		padding-top: 100px;
+		padding-right: 10%;
+		padding-bottom: 100px;
+		padding-left: 10%;
+	}
+	#contact h2 {
+		font-size: 40px;
+		line-height: 1.5em;
+	}
+	.right_column {
+		background-color: #414042;
+		padding-top: 100px;
+		padding-right: 10%;
+		padding-bottom: 100px;
+		padding-left: 10%;
+	}
+</style>
