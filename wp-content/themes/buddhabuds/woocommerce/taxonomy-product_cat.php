9ac1ad9e4e708ae7bda7b5227bec5b8379a8997c
Matt <matt@mspacecreative.com>
2020-03-21 12:15:16 -0300

    c

--- wp-content/themes/buddhabuds/woocommerce/taxonomy-product_cat.php
+++ wp-content/themes/buddhabuds/woocommerce/taxonomy-product_cat.php
@@ -19,6 +19,6 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-wc_get_template( 'archive-product.php' );
-
 include 'includes/contact-section.php';
+
+wc_get_template( 'archive-product.php' );
