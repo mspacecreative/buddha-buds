2715a0737bd1a73950a63df7d47ac07dd01f6fbe
Matt <matt@mspacecreative.com>
2020-03-23 17:25:29 -0300

    c

--- wp-content/themes/buddhabuds/functions/functions_out-of-stock.php
+++ wp-content/themes/buddhabuds/functions/functions_out-of-stock.php
@@ -6,3 +6,13 @@ function woocommerce_template_loop_stock() {
     if ( ! $product->managing_stock() && ! $product->is_in_stock() )
         echo '<p class="stock out-of-stock">Sold Out</p>';
 }
+
+add_filter( 'woocommerce_get_availability', 'wcs_custom_get_availability', 1, 2);
+function wcs_custom_get_availability( $availability, $_product ) {
+    
+    // Change Out of Stock Text
+    if ( ! $_product->is_in_stock() ) {
+        $availability['availability'] = __('Sold Out', 'woocommerce');
+    }
+    return $availability;
+}
