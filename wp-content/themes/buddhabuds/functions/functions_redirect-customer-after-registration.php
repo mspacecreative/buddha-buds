744dacdb466f307c26e7dee5cef9a6e83feb928e
Matt <matt@mspacecreative.com>
2020-03-27 08:32:20 -0300

    c

--- wp-content/themes/buddhabuds/functions/functions_redirect-customer-after-registration.php
+++ wp-content/themes/buddhabuds/functions/functions_redirect-customer-after-registration.php
@@ -10,10 +10,10 @@ function custom_redirection_after_registration( $redirection_url ){
 add_filter( 'woocommerce_registration_redirect', 'custom_redirection_after_registration', 10, 1 );
 
 // HIDE SUCCESS PAGE FROM PUBLIC
-add_filter( 'wp_head', function(){
+/*add_filter( 'wp_head', function(){
 	if (is_page('success') && !is_user_logged_in()) {
 		add_filter('the_content', function(){
 			return __('Sorry! Only registered customers are allowed to see this page. Please use the navigation above to find what you&#8217;re looking for.');
 		}, 99);
 	}
-});
+});*/
