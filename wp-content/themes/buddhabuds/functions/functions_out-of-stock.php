4cfbe867d7cbf9c89a377a04cd88a79d2d07265e
Matt <matt@mspacecreative.com>
2020-03-31 14:43:21 -0300

    c

--- wp-content/themes/buddhabuds/functions/functions_out-of-stock.php
+++ wp-content/themes/buddhabuds/functions/functions_out-of-stock.php
@@ -1,11 +1,11 @@
 <?php
-// display an 'Out of Stock' label on archive pages
+/*// display an 'Out of Stock' label on archive pages
 add_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_stock', 10 );
 function woocommerce_template_loop_stock() {
     global $product;
     if ( ! $product->managing_stock() && ! $product->is_in_stock() )
         echo '<p class="stock out-of-stock">Sold Out</p>';
-}
+}*/
 
 add_filter( 'woocommerce_get_availability', 'wcs_custom_get_availability', 1, 2);
 function wcs_custom_get_availability( $availability, $_product ) {
